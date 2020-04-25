b6e5bb206e9be30649543d689c86da55fb704c20
Naveen <enaveen98@gmail.com>
2020-04-25 17:53:01 +0530

    Updates

---
+++ precache-manifest.c5ba85b8260dc2dab1921cd1e734b286.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7f987105d648218088012eb6eb46f9",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "77ef70fe062629097f56",
+    "url": "/calculator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7ef8798e0099fa65cdf8",
+    "url": "/calculator/static/js/2.85204b0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.85204b0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77ef70fe062629097f56",
+    "url": "/calculator/static/js/main.1fa8d1e5.chunk.js"
+  },
+  {
+    "revision": "76b2aec7ab6e360446a5",
+    "url": "/calculator/static/js/runtime-main.a1af8741.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
